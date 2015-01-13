131d6866130257bf9eedc366e70e6948dcc942ab
WiredUK <wireduk@gmail.com>
2015-01-13 00:58:32 +0000

    Fixed changing between tabs to restore the icon message

--- background.js
+++ background.js
@@ -1,3 +1,24 @@
+var tabData = {};
+
 chrome.extension.onMessage.addListener(function(message, sender) {
     chrome.browserAction.setBadgeText({text: message});
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    	tabData["bs"+tabs[0].id] = message;
+	});
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	if(tabData["bs"+tabId.tabId]) {
+	   chrome.browserAction.setBadgeText({text: tabData["bs"+tabId.tabId]});
+	} else {
+		chrome.browserAction.setBadgeText({text: ""});
+	}
+});
+
+chrome.tabs.onActivated.addListener(function(tabId, changeInfo, tab) {
+	if(tabData["bs"+tabId.tabId]) {
+	   chrome.browserAction.setBadgeText({text: tabData["bs"+tabId.tabId]});
+	} else {
+		chrome.browserAction.setBadgeText({text: ""});
+	}
 });
--- popup.js
+++ popup.js
@@ -12,23 +12,35 @@ function saveOptions() {
 
 function restoreOptions() {
     chrome.storage.sync.get({
-        enabled: 'true'
+        enabled: 'true',
+        showtype: 0
     }, function(items) {
         document.getElementById('enabled').checked = items.enabled;
+        if(items.showtype == 0) {
+            document.getElementById('addsite').innerText = "Add this site to whitelist";
+        } else {
+            document.getElementById('addsite').innerText = "Add this site to blacklist";
+        }
     });
 }
 
 function addSite() {
+    var defaultSites = new Array();
+    defaultSites[0] = "http://.*";
+
     chrome.tabs.getSelected(null, function(tab) {
         chrome.storage.sync.get({
-            sites: new Array()
+            sites: defaultSites
         }, function(items) {
             items.sites.push(tab.url);
             chrome.storage.sync.set({sites: items.sites});
         });
 
     });
-    alert('Site added');
+    setTimeout(function() {
+        window.close();    
+    }, 1000);
+    
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
